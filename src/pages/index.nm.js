96ce44c46616c240d5c6a64de24363369addb739
vi a sang <sangvivi25@gmail.com>
2020-05-07 17:54:46 +0700

    Fix clients

--- src/pages/index.nm.js
+++ src/pages/index.nm.js
@@ -1,8 +1,9 @@
 const nav = require('../templates/includes/nav/nav');
+const clients = require('../templates/includes/clients/clients');
 delete require.cache[require.resolve('../templates/includes/nav/nav')];
 
 module.exports = {
   template: 'page',
   nav: nav,
-  foo: 'bar',
+  clients: clients,
 };
