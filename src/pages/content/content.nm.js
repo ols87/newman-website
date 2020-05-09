2d913199df80b4dcd948866a95c825507555fb8e
vi a sang <sangvivi25@gmail.com>
2020-05-09 22:42:20 +0700

    Copy content for page

--- src/pages/content/content.nm.js
+++ src/pages/content/content.nm.js
@@ -1,4 +1,19 @@
 module.exports = {
   template: 'page',
   slug: 'content-editing',
+  title: 'Content Editing',
+  quote:
+    'I help clients to produce pitch-perfect print publications and high-impact digital media content. Below are a few recent projects:',
+  content: [
+    {
+      title: 'EuroCham',
+      description: '<h5>The European Chamber of Commerce in Vietnam</h5>',
+      images: ['https://picsum.photos/id/57/200/300', 'https://picsum.photos/id/47/200/300', 'https://picsum.photos/id/46/200/300'],
+    },
+    {
+      title: 'Saigon-Hanoi Securities',
+      description: '<h5>Saigon-Hanoi Securities</h5>',
+      images: ['https://picsum.photos/id/57/200/300', 'https://picsum.photos/id/97/200/300'],
+    },
+  ],
 };
--- src/pages/copy/copy.nm.js
+++ src/pages/copy/copy.nm.js
@@ -3,14 +3,27 @@ module.exports = {
   slug: 'copywriting',
   title: 'Copywriting',
   quote:
-    'I provide strategic communications advice and practical media training to help clients promote campaigns, events, and ideas. Below are a few recent projects:',
+    'I help clients to communicate in engaging, effective, and persuasive language that speaks to their target audience. Below are a few recent projects:',
   content: [
     {
-      title: 'The British Embassy',
-      description: 'Media training workshops for the British Embassy in Hanoi',
-      body:
-        'I designed and delivered media training workshops for British Embassy staff in Hanoi. I taught skills including message development, pitching to journalists, and handling difficult questions and facilitated practical, on-camera interview practice.',
-      images: ['https://picsum.photos/id/77/1000/600'],
+      title: 'Austrade',
+      description: '<h5>The Australian Trade Commission</h5>',
+      images: ['https://picsum.photos/id/77/1000/600', 'https://picsum.photos/id/45/1000/600'],
+    },
+    {
+      title: 'Oxfam in Vietnam',
+      description: '<h5>Oxfam in Vietnam</h5>',
+      images: ['https://picsum.photos/id/37/1000/600'],
+    },
+    {
+      title: 'EuroCham',
+      description: '<h5>The European Chamber of Commerce in Vietnam</h5>',
+      images: ['https://picsum.photos/id/77/1000/600', 'https://picsum.photos/id/45/1000/600'],
+    },
+    {
+      title: 'Audier & Partners Law Firm',
+      description: '<h5>Audier & Partners Law Firm</h5>',
+      images: ['https://picsum.photos/id/45/1000/600'],
     },
   ],
 };
