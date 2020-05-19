a7105c06671788a908001b146d4abbbafcb492e9
vi a sang <sangvivi25@gmail.com>
2020-05-19 14:32:53 +0700

    Fix link in home page

--- src/pages/index.nm.js
+++ src/pages/index.nm.js
@@ -12,19 +12,19 @@ module.exports = {
       {
         name: 'Copywriting',
         content: 'I help clients to communicate in engaging, effective, and persuasive language that speaks to their target audience.',
-        link: '#',
-      },
-      {
-        name: 'Editing',
-        content:
-          'I help clients to produce pitch-perfect print and digital content in an original, professional, and consistent tone of voice..',
-        link: '#',
+        link: '/copywriting/',
       },
       {
         name: 'Consulting',
         content:
           'I provide strategic communications advice and practical media training to help clients define and deliver messages with impact.',
-        link: '#',
+        link: '/communications-consulting/',
+      },
+      {
+        name: 'Editing',
+        content:
+          'I help clients to produce pitch-perfect print and digital content in an original, professional, and consistent tone of voice..',
+        link: '/content-editing/',
       },
     ],
   },
