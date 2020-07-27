7b8972bf0a2344e01349c5c3d6573ce4a2f9138f
vi a sang <sangvivi25@gmail.com>
2020-07-27 14:16:46 +0700

    Remove old code

--- src/templates/includes/nav/nav.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-  { title: 'Home', slug: '/' },
-  { title: 'About', slug: '/about' },
-  { title: 'Copywriting', slug: '/copywriting' },
-  { title: 'Communications Consulting', slug: '/communications-consulting' },
-  { title: 'Content Editing', slug: '/content-editing' },
-  { title: 'Testimonials', slug: '/testimonials' },
-];
